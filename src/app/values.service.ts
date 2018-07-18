061a3cd3f7985b8177d8348af0df6342f2e2de61
karthikks3892 <32256341+karthikks3892@users.noreply.github.com>
2018-07-18 20:22:03 +0530

    18/7

--- src/app/values.service.ts
+++ src/app/values.service.ts
@@ -1,14 +1,9 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import { Subject } from 'rxjs/Subject';
-import { Resolve } from '@angular/router';
 
 @Injectable()
 export class ValuesService {
-  private myMethodSubject = new Subject<any>();
   value: any;
   Method(data) {
-    //this.myMethodSubject.next(data);
     this.value = data;
 
   }
